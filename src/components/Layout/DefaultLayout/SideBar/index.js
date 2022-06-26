c57f6cf414e919bdd85112aae311952b3238c1b0
Pham Duc Hai <haipdph11576@fpt.edu.vn>
2022-06-26 21:48:20 +0700

    khung DefaultLayout

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './SideBar.module.scss';
+
+const cx = classNames.bind(styles);
 
 function SideBar() {
-    return <div>SideBar</div>;
+    return <aside className={cx('wrapper')}>side bar</aside>;
 }
 
 export default SideBar;
